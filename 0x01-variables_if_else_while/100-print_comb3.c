8fa9083565ffd132f68dae8913fbf174cae49cb7
Your Name <saraamersaad8@gmail.com>
2023-03-17 15:18:07 -0700

    Inventing is a combination of brains and materials

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* more headers goes there */
+
+/**
+ *  main - Print random number last number
+ *  Return: 0 if success
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	/* your code goes there */
+	for (i = 0; i < 9; i++)
+	{
+		for (j = i + 1; j < 10 ; j++) 
+		{
+			putchar('0' + i);
+			putchar('0' + j);
+			if (i != 8)
+                	{
+                        	putchar(',');
+                        	putchar(' ');
+                	}
+
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
