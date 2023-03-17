09de92a8f5c44dc158d05b5c495a093b3a37b2d3
Your Name <saraamersaad8@gmail.com>
2023-03-17 14:51:55 -0700

    print numbers with comma

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* more headers goes there */
+
+/**
+ *  main - Print random number last number
+ *  Return: 0 if success
+ */
+int main(void)
+{
+	int i;
+
+	/* your code goes there */
+	for (i = 0; i < 10; i++)
+	{
+		putchar('0' + i);
+		if (i == 9)
+		{
+			putchar('$');
+		} else
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	return (0);
+}
