f8b4d4f2f2874b376ecf8b7d62d871aa92ad1f41
Your Name <saraamersaad8@gmail.com>
2023-03-17 13:06:39 -0700

    fix betty style

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,11 +4,15 @@
 
 /* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ *  main - Print random number last number
+ *  Return: 0 if success
+ */
 int main(void)
 {
 	int n;
-        int t;
+	int t;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	t = n % 10;
@@ -20,9 +24,9 @@ int main(void)
 	} else if (t == 0)
 	{
 		printf(" and is 0");
-	} else if(t < 6)
+	} else if (t < 6)
 	{
-	 	printf(" and is less than 6 and not 0");   
+		printf(" and is less than 6 and not 0");
 	}
 
 	return (0);
