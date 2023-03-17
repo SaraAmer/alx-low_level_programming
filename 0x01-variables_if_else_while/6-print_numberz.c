17b26b05370b314977e0def451a890755000f687
Your Name <saraamersaad8@gmail.com>
2023-03-17 14:21:11 -0700

    print integers by putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* more headers goes there */
+
+/**
+ *  main - Print random number last number
+ *  Return: 0 if success
+ */
+int main(void)
+{
+	int i;
+
+	/* your code goes there */
+	for (i = 0; i < 10; i++)
+	{
+		putchar('0' + i);
+	}
+	putchar('\n');
+
+	return (0);
+}
