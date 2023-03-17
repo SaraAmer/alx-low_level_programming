49c87aa2d679de487778cbbe67a806ec11be4aae
Your Name <saraamersaad8@gmail.com>
2023-03-17 14:54:57 -0700

    print numbers with comma

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -16,14 +16,11 @@ int main(void)
 	for (i = 0; i < 10; i++)
 	{
 		putchar('0' + i);
-		if (i == 9)
-		{
-			putchar('$');
-		} else
+		if (i != 9)
 		{
 			putchar(',');
-			putchar(' ');
 		}
+		putchar(' ');
 	}
 
 	return (0);
