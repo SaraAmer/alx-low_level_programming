b21ff84e6e4150169e4c30f060638098859ed9b0
Your Name <saraamersaad8@gmail.com>
2023-03-17 15:25:08 -0700

    fix betty style

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -16,15 +16,15 @@ int main(void)
 	/* your code goes there */
 	for (i = 0; i < 9; i++)
 	{
-		for (j = i + 1; j < 10 ; j++) 
+		for (j = i + 1; j < 10 ; j++)
 		{
 			putchar('0' + i);
 			putchar('0' + j);
 			if (i != 8)
-                	{
-                        	putchar(',');
-                        	putchar(' ');
-                	}
+			{
+				putchar(',');
+				putchar(' ');
+			}
 
 		}
 	}
