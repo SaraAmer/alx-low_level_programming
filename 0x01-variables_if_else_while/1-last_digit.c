40eb05eba353f6741e0079bc700e9b08d21fc405
Your Name <saraamersaad8@gmail.com>
2023-03-17 13:08:57 -0700

    fix betty style

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,13 +20,13 @@ int main(void)
 	printf("Last digit of %d is %d", n, t);
 	if (t > 5)
 	{
-		printf(" and is greater than 5");
+		printf(" and is greater than 5\n");
 	} else if (t == 0)
 	{
-		printf(" and is 0");
+		printf(" and is 0\n");
 	} else if (t < 6)
 	{
-		printf(" and is less than 6 and not 0");
+		printf(" and is less than 6 and not 0\n");
 	}
 
 	return (0);
